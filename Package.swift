8fb0c2a6f8ea20616b11bb74174cab80826f60f8
Hiroshi Kimura <muukii.app@gmail.com>
2024-10-10 17:26:38 +0900

    Update Package.swift (#22)

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
   name: "swift-dynamic-list",
-  platforms: [.iOS(.v14)],
+  platforms: [.iOS(.v16)],
   products: [
     // Products define the executables and libraries a package produces, making them visible to other packages.
     .library(
