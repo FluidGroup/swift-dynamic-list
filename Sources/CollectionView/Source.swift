93c0bbd1af26df1c4c1e459e0376c0b42b0f55c6
Muukii <muukii.app@gmail.com>
2024-10-27 20:48:31 +0900

    Update

--- Sources/CollectionView/Source.swift
+++ Sources/CollectionView/Source.swift
@@ -6,6 +6,9 @@ public enum CollectionViewListDirection {
   case horizontal
 }
 
+/**
+ A protocol that makes laid out contents of the collection view
+ */
 public protocol CollectionViewLayoutType: ViewModifier {
   
 }
@@ -76,7 +79,7 @@ public enum CollectionViewLayouts {
   public struct Grid: CollectionViewLayoutType {
     
     public func body(content: Content) -> some View {
-      
+      // FIXME:
     }
   }
   
@@ -112,9 +115,36 @@ extension CollectionViewLayoutType where Self == CollectionViewLayouts.Grid {
   
 }
 
-public enum SelectionMode {
-  case single
-  case multiple
+public enum SelectAction {
+  case selected
+  case deselected
+}
+
+/// 
+public enum Selection<Item: Identifiable> {
+  
+  /**
+   Single selection
+   
+   - Parameters:
+   - selected:
+   - onChange: passing newly selected item, nil represents as deselected.
+   */
+  case single(
+    selected: Item.ID?,
+    onChange: (_ selected: Item?) -> Void)
+  
+  /**
+   Multiple selection
+   
+   - Parameters:
+     - selected:
+     - onChange: passing newly selected item, nil represents as deselected.
+   */
+  case multiple(
+    selected: Set<Item.ID>,
+    onChange: (_ selected: Item, _ action: SelectAction) -> Void
+  )
 }
 
 /// Still searching better name
@@ -131,7 +161,9 @@ public struct CollectionView<
   private let cell: (Data.Index, Data.Element) -> Cell
   private let layout: Layout
   private let items: Data
-  
+    
+  private var selection: Selection<Data.Element>?
+    
   public init(
     items: Data,
     direction: CollectionViewListDirection,
@@ -149,12 +181,61 @@ public struct CollectionView<
     
     // for now, switching verbose way
     
-    ForEach(IndexedCollection(items)) { element in      
+    ForEach(IndexedCollection(items)) { element in  
+      
+      let isSelected = {
+        switch selection {
+        case .none: 
+          return false
+        case .single(let id, _):
+          return element.id == id
+        case .multiple(let set, _):
+          return set.contains(element.id)
+        }
+      }()
+      
       cell(element.index, element.value)
+        .environment(\.collectionView_isSelected, isSelected)
+        .environment(\.collectionView_updateSelection, { [selection] isSelected in 
+          
+          switch (selection) {
+          case (.single(_, let onChange)):
+            if isSelected {
+              onChange(element.value)
+            } else {
+              onChange(nil)
+            }
+          case .multiple(_, let onChange):
+            if isSelected {
+              onChange(element.value, .selected)
+            } else {
+              onChange(element.value, .deselected)
+            }
+          default:
+            break
+          }
+        })
     }
     .modifier(layout)
             
   }
+  
+  public consuming func selection(
+    _ selection: Selection<Data.Element>
+  ) -> Self {    
+    
+    self.selection = selection
+    
+    return self
+  }
+}
+
+extension EnvironmentValues {
+  @Entry var collectionView_isSelected: Bool = false
+}
+
+extension EnvironmentValues {
+  @Entry var collectionView_updateSelection: (Bool) -> Void = { _ in }
 }
 
 #if DEBUG
@@ -164,29 +245,98 @@ private struct Item: Identifiable {
   var title: String
   
   static func mock() -> [Item] {
-    return (0..<10).map { index in
+    return (0..<1000).map { index in
       Item(id: index, title: "Item \(index)")
     }
   }
 }
 
-#Preview("Custom List") {
+private struct Cell: View {
   
-  CollectionView(
-    items: Item.mock(), direction: .vertical,
-    layout: .list {
-      RoundedRectangle(cornerRadius: 8)
-        .fill(.secondary)
-        .frame(height: 8)
-        .padding(.horizontal, 20)      
-    },
-    cell: { index, item in
-      HStack {
-        Text(index.description)
-        Text(item.title)
-      }
+  @Environment(\.collectionView_isSelected) var isSelected
+  @Environment(\.collectionView_updateSelection) var updateSelection
+  
+  let index: Int
+  let item: Item
+      
+  var body: some View {
+    HStack {
+      Circle()
+        .fill(.red)
+        .frame(width: 20, height: 20)
+        .opacity(isSelected ? 1 : 0.2)
+      Text(index.description)
+      Text(item.title)
     }
-  )
+    ._onButtonGesture(pressing: { _ in }, perform: {
+      updateSelection(!isSelected)
+    })
+  }
+}
+
+#Preview("Custom List / Single selection") {
+  
+  struct Book: View {
+    
+    @State var selected: Item?
+    
+    var body: some View {
+      CollectionView(
+        items: Item.mock(), direction: .vertical,
+        layout: .list {
+          RoundedRectangle(cornerRadius: 8)
+            .fill(.secondary)
+            .frame(height: 8)
+            .padding(.horizontal, 20)      
+        },
+        cell: { index, item in
+          Cell(index: index, item: item)
+        }
+      )
+      .selection(.single(
+        selected: selected?.id,
+        onChange: { e in
+          selected = e
+      }))
+    }
+  }
+
+  return Book()
+}
+
+#Preview("Custom List / Multiple selection") {
+  
+  struct Book: View {
+    
+    @State var selected: Set<Item.ID> = .init()
+    
+    var body: some View {
+      CollectionView(
+        items: Item.mock(), direction: .vertical,
+        layout: .list {
+          RoundedRectangle(cornerRadius: 8)
+            .fill(.secondary)
+            .frame(height: 8)
+            .padding(.horizontal, 20)      
+        },
+        cell: { index, item in
+          Cell(index: index, item: item)
+        }
+      )
+      .selection(.multiple(
+        selected: selected,
+        onChange: { e, action in
+          switch action {
+          case .selected:
+            selected.insert(e.id)
+          case .deselected:
+            selected.remove(e.id)
+          }
+        }))
+    }
+  }
+  
+  return Book()
 }
 
 #Preview("SwiftUI List") {
@@ -201,6 +351,43 @@ private struct Item: Identifiable {
       }
     }
   )
+}
+
+#Preview {
+  
+  struct BookList: View {
+    
+    struct Ocean: Identifiable, Hashable {
+      let name: String
+      let id = UUID()
+    }
+    
+    
+    private var oceans = [
+      Ocean(name: "Pacific"),
+      Ocean(name: "Atlantic"),
+      Ocean(name: "Indian"),
+      Ocean(name: "Southern"),
+      Ocean(name: "Arctic")
+    ]
+    
+    
+    @State private var multiSelection = Set<UUID>()
+        
+    var body: some View {
+      NavigationView {
+        List(oceans, selection: $multiSelection) {
+          Text($0.name)
+        }
+        .navigationTitle("Oceans")
+//        .toolbar { EditButton() }
+      }
+      Text("\(multiSelection.count) selections")
+    }
+    
+  }
+  
+  return BookList()
 }
 
 #endif
