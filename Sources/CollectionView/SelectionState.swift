108a742472fa50290d83ac16588a186d2a08b477
Hiroshi Kimura <muukii.app@gmail.com>
2025-01-19 17:33:04 +0900

    Reduce frequency of view update (#45)

--- Sources/CollectionView/SelectionState.swift
+++ Sources/CollectionView/SelectionState.swift
@@ -25,15 +25,15 @@ extension SelectionState {
     
     let isSelected: Bool = isSelected(for: identifier)
     let isDisabled: Bool = !isEnabled(for: identifier)
-    
+
     return body
       .disabled(isDisabled)
       .environment(\.collectionView_isSelected, isSelected)
       .environment(
         \.collectionView_updateSelection,
-         { isSelected in
+         .init(handler: { isSelected in
            self.update(isSelected: isSelected, for: identifier)
-         }
+         })
       )    
   }
   
